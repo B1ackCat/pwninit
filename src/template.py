a24d9fb202a975c34e81c4fd98a93ce53a2cfb66
B1ackCat <b1ack3at.dev@gmail.com>
2026-02-01 19:14:32 +0900

    add helper

--- src/template.py
+++ src/template.py
@@ -2,20 +2,31 @@ from pwn import *
 context.terminal = ["tmux", "split", "-h"]
 context.arch = "amd64"
 
-# log helper
-slog = lambda k, v: log.info(f"{{k}} = {{v}}")
+# ---------------- helpers ----------------
+def start(e):
+    # ./exp.py <host> <port>
+    if len(sys.argv) >= 3:
+        host, port = sys.argv[1], sys.argv[2]
+        log.info(f"Running remote @ {{host}}:{{port}}...")
+        return remote(host, int(port))
 
-# pwntools send/recv helper
-_b = lambda x: x if isinstance(x, bytes) else str(x).encode()
-sl = lambda d, x: p.sendlineafter(_b(d), _b(x))
-sa = lambda d, x: p.sendafter(_b(d), _b(x))
-ru = lambda d: p.recvuntil(_b(d))
-rl = lambda: p.recvline()
-rv = lambda: p.recv()
+    log.info(f"Running local...")
+    return process(e.path)
+
+lg = lambda k, v: log.info(f"{{k}} = {{v}}")
+_b  = lambda x: x if isinstance(x, bytes) else str(x).encode()
+s   = lambda x: p.send(_b(x))
+sl  = lambda x: p.sendline(_b(x))
+sa  = lambda d, x: p.sendafter(_b(d), _b(x))
+sla = lambda d, x: p.sendlineafter(_b(d), _b(x))
+ru  = lambda d: p.recvuntil(_b(d))
+rl  = lambda: p.recvline()
+rv  = lambda: p.recv()
 rvn = lambda n: p.recvn(n)
 
+# ---------------- exploit ----------------
 {bindings}
 libc = e.libc
-p = process(e.path)
+p = start(e)
 
 p.interactive()
